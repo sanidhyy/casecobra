70e3c1a6f1f443a52a48ec8dc1da309105303eb7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-15 05:27:55 +0000

    steps ui completed

--- app/configure/layout.tsx
+++ app/configure/layout.tsx
@@ -1,10 +1,13 @@
 import type { PropsWithChildren } from "react";
 
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+import { Steps } from "@/components/steps";
 
 const ConfigureLayout = ({ children }: PropsWithChildren) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
+
       {children}
     </MaxWidthWrapper>
   );
---
+++ components/steps.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const STEPS = [
+  {
+    name: "Step 1: Add image",
+    description: "Choose an image for your case.",
+    url: "/upload",
+  },
+  {
+    name: "Step 2: Customize design",
+    description: "Make the case yours.",
+    url: "/design",
+  },
+  {
+    name: "Step 3: Summary",
+    description: "Review your final design.",
+    url: "/preview",
+  },
+];
+
+export const Steps = () => {
+  const pathname = usePathname();
+
+  return (
+    <ol className="rounded-md bg-white lg:flex lg:rounded-none lg:border-l lg:border-r lg:border-gray-200">
+      {STEPS.map((step, i) => {
+        const isCurrent = pathname.endsWith(step.url);
+        const isCompleted = STEPS.slice(i + 1).some((step) =>
+          pathname.endsWith(step.url)
+        );
+        const imgPath = `/snake-${i + 1}.png`;
+
+        return (
+          <li key={step.name} className="relative overflow-hidden lg:flex-1">
+            <div>
+              <span
+                aria-hidden
+                className={cn(
+                  "absolute left-0 top-0 h-full w-1 bg-zinc-400 lg:bottom-0 lg:top-auto lg:h-1 lg:w-full",
+                  {
+                    "bg-zinc-700": isCurrent,
+                    "bg-primary": isCompleted,
+                  }
+                )}
+              />
+
+              <span
+                className={cn(
+                  "flex items-center px-6 py-4 text-sm",
+                  i !== 0 ? "lg:pl-9" : ""
+                )}
+              >
+                <span className="flex shrink-0">
+                  <Image
+                    src={imgPath}
+                    alt={step.name}
+                    height={80}
+                    width={80}
+                    className={cn(
+                      "flex h-20 w-20 object-contain items-center justify-center",
+                      {
+                        "border-none": isCompleted,
+                        "border-zinc-700": isCurrent,
+                      }
+                    )}
+                  />
+                </span>
+
+                <span className="ml-4 h-full mt-0.5 flex min-w-0 flex-col justify-center">
+                  <span
+                    className={cn("text-sm font-semibold text-zinc-700", {
+                      "text-primary": isCompleted,
+                      "text-zinc-700": isCurrent,
+                    })}
+                  >
+                    {step.name}
+                  </span>
+
+                  <span className="text-sm text-zinc-500">
+                    {step.description}
+                  </span>
+                </span>
+              </span>
+
+              {/* separator */}
+              {i !== 0 ? (
+                <div
+                  aria-hidden
+                  className="absolute inset-0 hidden w-3 lg:block"
+                >
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    viewBox="0 0 12 82"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0.5 0V31L10.5 41L0.5 51V82"
+                      stroke="currentcolor"
+                      vectorEffect="non-scaling-stroke"
+                    />
+                  </svg>
+                </div>
+              ) : null}
+            </div>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
