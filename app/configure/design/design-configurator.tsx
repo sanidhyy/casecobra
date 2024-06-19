b73f7ff9d0bc72e5c1722817421bc1a544cdfe68
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-19 07:16:41 +0000

    model dropdown implemented

--- app/configure/design/design-configurator.tsx
+++ app/configure/design/design-configurator.tsx
@@ -7,10 +7,18 @@ import { Rnd } from "react-rnd";
 
 import { HandleComponent } from "@/components/handle-component";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
-import { COLORS } from "@/validators/option-validator";
+import { COLORS, MODELS } from "@/validators/option-validator";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 type DesignConfiguratorProps = {
   configId: string;
@@ -28,8 +36,10 @@ export const DesignConfigurator = ({
 }: DesignConfiguratorProps) => {
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number];
+    model: (typeof MODELS.options)[number];
   }>({
     color: COLORS[0],
+    model: MODELS.options[0],
   });
 
   return (
@@ -103,42 +113,92 @@ export const DesignConfigurator = ({
             <div aria-hidden className="w-full h-px bg-zinc-200 my-6" />
 
             <div className="relative mt-4 h-full flex flex-col justify-between">
-              <RadioGroup
-                value={options.color}
-                onChange={(newColor) => {
-                  setOptions((prevColor) => ({
-                    ...prevColor,
-                    color: newColor,
-                  }));
-                }}
-              >
-                <Label>Color: {options.color.label}</Label>
+              <div className="flex flex-col gap-6">
+                <RadioGroup
+                  value={options.color}
+                  onChange={(newColor) => {
+                    setOptions((prevColor) => ({
+                      ...prevColor,
+                      color: newColor,
+                    }));
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
 
-                <div className="mt-3 flex items-center space-x-3">
-                  {COLORS.map((color) => (
-                    <Radio
-                      key={color.label}
-                      value={color}
-                      className={({ checked }) =>
-                        cn(
-                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
-                          {
-                            [`border-${color.tw}`]: checked,
-                          }
-                        )
-                      }
-                    >
-                      <span
-                        aria-hidden
-                        className={cn(
-                          `bg-${color.tw}`,
-                          "h-8 w-8 rounded-full border border-black border-opacity-10"
-                        )}
-                      />
-                    </Radio>
-                  ))}
+                  <div className="mt-3 flex items-center space-x-3">
+                    {COLORS.map((color) => (
+                      <Radio
+                        key={color.label}
+                        value={color}
+                        className={({ checked }) =>
+                          cn(
+                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                            {
+                              [`border-${color.tw}`]: checked,
+                            }
+                          )
+                        }
+                      >
+                        <span
+                          aria-hidden
+                          className={cn(
+                            `bg-${color.tw}`,
+                            "h-8 w-8 rounded-full border border-black border-opacity-10"
+                          )}
+                        />
+                      </Radio>
+                    ))}
+                  </div>
+                </RadioGroup>
+
+                <div className="relative flex flex-col gap-3 w-full">
+                  <Label>Model</Label>
+
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className="w-full justify-between"
+                      >
+                        {options.model.label}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent>
+                      {MODELS.options.map((model) => (
+                        <DropdownMenuItem
+                          key={model.value}
+                          className={cn(
+                            "flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100",
+                            {
+                              "bg-primary-foreground text-primary":
+                                model.value === options.model.value,
+                            }
+                          )}
+                          onClick={() => {
+                            setOptions((prevOptions) => ({
+                              ...prevOptions,
+                              model,
+                            }));
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              model.value === options.model.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {model.label}
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-              </RadioGroup>
+              </div>
             </div>
           </div>
         </ScrollArea>
--- validators/option-validator.ts
+++ validators/option-validator.ts
@@ -21,3 +21,33 @@ export const COLORS = [
     tw: "rose-950",
   },
 ] as const;
+
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+  ],
+} as const;
