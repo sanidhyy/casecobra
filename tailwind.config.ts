2e4f7c2c6dd279c85f294a37897293c736b153f6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-19 14:10:11 +0000

    loadiing states added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,10 +80,15 @@ const config = {
             opacity: "1",
           },
         },
+        flashing: {
+          "0%, 100%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+        },
       },
       animation: {
         marquee: "marquee var(--marquee-duration) linear infinite",
         "fade-in": "fade-in 0.5s linear forwards",
+        flashing: "flashing 1.4s infinite linear",
       },
     },
   },
