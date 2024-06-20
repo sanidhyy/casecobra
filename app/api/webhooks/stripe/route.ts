3a1b0a9969dd65b10f94f5e743b6495c7d55219a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-20 09:10:47 +0000

    stripe webhook implemented

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,82 @@
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+import { stripe } from "@/lib/stripe";
+import { db } from "@/db";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.text();
+    const signature = headers().get("stripe-signature");
+
+    if (!signature)
+      return new NextResponse("Invalid signature.", { status: 400 });
+
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+
+    if (event.type === "checkout.session.completed") {
+      if (!event.data.object.customer_details?.email)
+        throw new Error("Missing user email.");
+
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      const { userId, orderId } = session.metadata || {
+        userId: null,
+        orderId: null,
+      };
+
+      if (!userId || !orderId) throw new Error("Invalid requests metadata.");
+
+      const billingAddress = session.customer_details!.address;
+      const shippingAddress = session.shipping_details!.address;
+
+      await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details?.name!,
+              city: shippingAddress?.city!,
+              country: shippingAddress?.country!,
+              postalCode: shippingAddress?.postal_code!,
+              street: shippingAddress?.line1!,
+              state: shippingAddress?.state!,
+            },
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details?.name!,
+              city: billingAddress?.city!,
+              country: billingAddress?.country!,
+              postalCode: billingAddress?.postal_code!,
+              street: billingAddress?.line1!,
+              state: billingAddress?.state!,
+            },
+          },
+        },
+      });
+    }
+
+    return NextResponse.json({ result: event, ok: true }, { status: 200 });
+  } catch (error) {
+    console.error("STRIPE_WEBHOOK: ", error);
+
+    return NextResponse.json(
+      {
+        message: "Something went wrong!",
+        ok: false,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
