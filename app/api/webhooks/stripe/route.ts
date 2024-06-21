ee076fbead094441138f2a5fb142f634fb0159e0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-21 09:37:16 +0000

    order received email implemented

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,9 +1,13 @@
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 import Stripe from "stripe";
 
 import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
+import { OrderReceivedEmail } from "@/components/emails/order-received-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   try {
@@ -35,7 +39,7 @@ export async function POST(req: NextRequest) {
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -62,6 +66,26 @@ export async function POST(req: NextRequest) {
             },
           },
         },
+      });
+
+      await resend.emails.send({
+        from: `casecobra <${process.env.RESEND_SENDER_EMAIL}>`,
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          shippingAddress: {
+            id: orderId,
+            name: session.customer_details?.name!,
+            city: shippingAddress?.city!,
+            country: shippingAddress?.country!,
+            postalCode: shippingAddress?.postal_code!,
+            street: shippingAddress?.line1!,
+            state: shippingAddress?.state!,
+            phone: null,
+          },
+        }),
       });
     }
 
---
+++ components/emails/order-received-email.tsx
@@ -0,0 +1,229 @@
+import type { ShippingAddress } from "@prisma/client";
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+
+type OrderReceivedEmailProps = {
+  shippingAddress: ShippingAddress;
+  orderId: string;
+  orderDate: string;
+};
+
+export const OrderReceivedEmail = ({
+  shippingAddress,
+  orderId,
+  orderDate,
+}: OrderReceivedEmailProps) => {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_BASE_URL;
+
+  return (
+    <Html>
+      <Head />
+      <Preview>Your order summary and estimated delivery date</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={message}>
+            <Img
+              src={`${baseUrl}/snake-3.png`}
+              alt="Delivery Snake"
+              width={65}
+              height={73}
+              style={{ margin: "auto" }}
+            />
+
+            <Heading style={global.heading}>Thank your for your order!</Heading>
+
+            <Text style={global.text}>
+              We&apos;re preparing everything for delivery and will notify you
+              once your package has been shipped. Delivery usually takes 2 days.
+            </Text>
+
+            <Text
+              style={{
+                ...global.text,
+                marginTop: 24,
+              }}
+            >
+              If you have any questions regarding your order, please feel free
+              to contact us with your order number and we&apos;re here to help.
+            </Text>
+          </Section>
+
+          <Hr style={global.hr} />
+
+          <Section style={global.defaultPadding}>
+            <Text style={addressTitle}>
+              Shipping to: {shippingAddress.name}
+            </Text>
+            <Text
+              style={{
+                ...global.text,
+                fontSize: 14,
+              }}
+            >
+              {shippingAddress.street}, {shippingAddress.city},{" "}
+              {shippingAddress.state} - {shippingAddress.postalCode}
+            </Text>
+          </Section>
+
+          <Hr style={global.hr} />
+
+          <Section style={global.defaultPadding}>
+            <Row
+              style={{
+                display: "inline-flex",
+                marginBottom: 40,
+              }}
+            >
+              <Column style={{ width: 170 }}>
+                <Text style={global.paragraphWithBold}>Order Number</Text>
+                <Text style={track.number}>{orderId}</Text>
+              </Column>
+
+              <Column>
+                <Text style={global.paragraphWithBold}>Order Date</Text>
+                <Text style={track.number}>{orderDate}</Text>
+              </Column>
+            </Row>
+          </Section>
+
+          <Hr style={global.hr} />
+
+          <Section style={paddingY}>
+            <Row>
+              <Text
+                style={{
+                  ...footer.text,
+                  paddingTop: 30,
+                  paddingBottom: 30,
+                }}
+              >
+                Please contact us if you have any questions. (if you reply to
+                this email, we won&apos;t be able to see it.)
+              </Text>
+            </Row>
+
+            <Row>
+              <Text style={footer.text}>
+                &copy; CaseCobra, Inc. All Rights Reserved.
+              </Text>
+            </Row>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+const paddingX = {
+  paddingLeft: "40px",
+  paddingRight: "40px",
+};
+
+const paddingY = {
+  paddingTop: "22px",
+  paddingBottom: "22px",
+};
+
+const paragraph = {
+  margin: "0",
+  lineHeight: "2",
+};
+
+const global = {
+  paddingX,
+  paddingY,
+  defaultPadding: {
+    ...paddingX,
+    ...paddingY,
+  },
+  paragraphWithBold: { ...paragraph, fontWeight: "bold" },
+  heading: {
+    fontSize: "32px",
+    lineHeight: "1.3",
+    fontWeight: "700",
+    textAlign: "center",
+    letterSpacing: "-1px",
+  } as React.CSSProperties,
+  text: {
+    ...paragraph,
+    color: "#747474",
+    fontWeight: "500",
+  },
+  button: {
+    border: "1px solid #929292",
+    fontSize: "16px",
+    textDecoration: "none",
+    padding: "10px 0px",
+    width: "220px",
+    display: "block",
+    textAlign: "center",
+    fontWeight: 500,
+    color: "#000",
+  } as React.CSSProperties,
+  hr: {
+    borderColor: "#E5E5E5",
+    margin: "0",
+  },
+};
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "10px auto",
+  width: "600px",
+  maxWidth: "100%",
+  border: "1px solid #E5E5E5",
+};
+
+const track = {
+  container: {
+    padding: "22px 40px",
+    backgroundColor: "#F7F7F7",
+  },
+  number: {
+    margin: "12px 0 0 0",
+    fontWeight: 500,
+    lineHeight: "1.4",
+    color: "#6F6F6F",
+  },
+};
+
+const message = {
+  padding: "40px 74px",
+  textAlign: "center",
+} as React.CSSProperties;
+
+const addressTitle = {
+  ...paragraph,
+  fontSize: "15px",
+  fontWeight: "bold",
+};
+
+const footer = {
+  policy: {
+    width: "166px",
+    margin: "auto",
+  },
+  text: {
+    margin: "0",
+    color: "#AFAFAF",
+    fontSize: "13px",
+    textAlign: "center",
+  } as React.CSSProperties,
+};
