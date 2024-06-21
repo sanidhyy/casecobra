3fdec7f60b5c4c1a9ae7acd82b9e23447f26c1bc
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-06-21 16:00:56 +0000

    style typo fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-[100dvh] px-4 md:px-6">
+    <div className="flex flex-col items-center justify-center h-[70dvh] px-4 md:px-6">
       <Image
         src="/snake-3.png"
         width={200}
