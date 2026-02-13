b65dfd0324fb0c61123447b3033d6ef2f5279fab
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2026-02-13 10:26:40 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -118,7 +118,7 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: "@sanidhyyy",
+      creator: "@_sanidhyy",
     },
     metadataBase: new URL(process.env.NEXT_PUBLIC_APP_BASE_URL),
   };
